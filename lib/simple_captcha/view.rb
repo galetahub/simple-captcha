4afcea919987c7ebfaf10b69723081905914a8e7
zhangdabei <keepyouliking@gmail.com>
2012-03-27 13:13:59 +0800

    Update lib/simple_captcha/view.rb

--- lib/simple_captcha/view.rb
+++ lib/simple_captcha/view.rb
@@ -64,7 +64,7 @@ module SimpleCaptcha #:nodoc
         query = defaults.collect{ |key, value| "#{key}=#{value}" }.join('&')
         url = "/simple_captcha?code=#{simple_captcha_key}&#{query}"
         #the url should be encoded,or it can not be userd in wml document
-        URI.encode("<img src='#{url}' alt='captcha' />".html_safe)
+        "<img src='#{URI.encode(url)}' alt='captcha' />".html_safe
       end
       
       def simple_captcha_field(options={})
